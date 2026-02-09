78d5db2916aad1e8fd1882ce80cc059d235d3d29
hotlatteiceamericano <hotlatteiceamericano@gmail.com>
2026-02-09 23:46:35 +0800

    misc

--- src/store/object_store.rs
+++ src/store/object_store.rs
@@ -1,7 +1,7 @@
 use std::{path::PathBuf, pin::Pin};
 
 use axum::body::Bytes;
-use futures::{Stream, io, stream::BoxStream};
+use futures::{Stream, io};
 
 use crate::common::store_type::StoreType;
 
@@ -11,14 +11,9 @@ pub trait ObjectStore {
     fn path() -> PathBuf;
     async fn save(&mut self, bytes: &Bytes) -> anyhow::Result<StoreType>;
 
-    // I think the outer Result wrap is confusing but still needed
-    // outer Result meant for other errors like file opening
-    // inner error meant for the error to read each chunks
-    // todo: see how this can be improved
     /// Purposefully return BoxStream instead of axum::IntoResponse
     /// to decouple the store layer from http layer
     async fn open(
         file_name: &str,
-        // ) -> anyhow::Result<BoxStream<'static, Result<Bytes, io::Error>>>;
     ) -> anyhow::Result<Pin<Box<dyn Stream<Item = Result<Bytes, io::Error>> + Send>>>;
 }
