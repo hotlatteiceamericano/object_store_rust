dbce92b41e80fbaeaf72c9acf07c58b314cb1c92
hotlatteiceamericano <hotlatteiceamericano@gmail.com>
2026-02-11 23:59:19 +0800

    an idea to prevent the mismatch of Storable's content length

--- src/store/blob.rs
+++ src/store/blob.rs
@@ -15,6 +15,10 @@ impl Blob {
 impl Storable for Blob {
     // how can I prevent this bug?
     // Segment::write calculate the length of serializeed instance
+    // may be different from the implementation here
+    // one thought:
+    // Storable to have a serialize method and ask concrete struct to implement
+    // then content_length calls self.serialize().len()
     fn content_length(&self) -> u32 {
         bincode::serialize(&self).unwrap().len() as u32
     }
